dfd5db9b757226a7399ab7469dfe9140bcc78ed3
choncba <luciano_bono@hotmail.com>
2024-03-22 18:02:53 -0300

    collector

--- Curso.md
+++ Curso.md
@@ -1,44 +1,7 @@
-# Curso Opentelemetry Udemy - Parte 1
+# Curso Opentelemetry Udemy - Parte 2
 https://tecom.udemy.com/course/observability-in-cloud-native-apps-using-opentelemetry/
-Repo: https://github.com/habmic/opentelemetry-101/tree/main
+Repo origen: https://github.com/habmic/opentelemetry-101/tree/main
+Fork: https://github.com/choncba/opentelemetry-udemy
 
-## La parte 2 del curso es usando el colector de opentelemetry, ir al branch opentelemetry-collector
-
-## Pasos
-1 - Instalar módulos localmente ejecutando: yarn
-  - Correr docker-compose up -> En este punto no hay telemetría instalada
-  - Test: http://localhost:8081/todos
-
-2 - Instalar Opentelemetry, Jaeger, y crear tracer.ts [ver](./docs/installing-opentelemetry.md)
-  - Correr docker-compose up -> En este punto tenemos telemetría sólo de traces con OpenTelemetry
-  - Jaeger: http://localhost:16686/search
-
-3 - Agregamos métricas con prometheus [ver](./docs/adding-metrics.md)
-  - Prometheus: http://localhost:9090/graph -> Al punto anterior, sumamos métricas
-
-4 - Correlación de Traces y Logs [ver](./docs/correlate-logs-with-traces.md)
-  - Cuando llamemos a http://localhost:8081/todos?fail=1 se va a generar un error 500 que podemos ver correlacionado con el trace en los logs y en Jaeger
-
-5 - Creación manual de Spans [ver](./docs/creating-manual-spans.md)
-  - De esta forma generamos un Span de forma manual para el código que se ejecuta dentro, y lo vemos en Jaeger como operation: Set default items
-
-6 - Atributos personalizados en los Spans (Tags en Jaeger) [ver](./docs/adding-custom-attributes.md)
-  - Nos permite agregar atributos adicionales a los generados automáticamente por Opentelemetry en los Spans, por ej. un nombre de usuario
-
-7 - Configuración de instrumentaciones: Cada instrumentación puede configurarse según los parámetros que encontramos en la [documentación](https://opentelemetry.io/ecosystem/registry/?language=js&component=instrumentation). Es posible incluso desactivar módulos de instrumentación automática que no deseamos incorporar en los spans, ver [tracer.ts](./tracer.ts)
-
-8 - Logs Debug [ver](./docs/debug-logs.md) -> Habilita los logs de opentelemetry en la consola con el nivel seteado (DEBUG/ERROR/WARNING, etc.)
-
-9 - Resources: agregan en los Span información sobre el entorno en que se ejecuta la aplicación de forma automática (Campo Process en Jaeger), además podemos agregar entradas personalizadas [ver](./docs/define-custom-resources.md)
-
-10 - Sampling: Por defecto, Opentelemetry toma muestra de todo lo instrumentado en la aplicación, es decir, genera Spans para cualquier librería compatible instrumentada. Esto hace que por ejemplo, las llamadas desde Prometheus para relevar métricas, también generen traces/spans, y esto no es deseado en producción. [Ver](./docs/configure-sampling.md)
-
-11 - Context Propagation: Esta funcionalidad nos permite propagar el contexto de opentelemetry de un Span a otro, mediante 3 estándares: W3C, B3, o Jaeger. Por defecto usamos W3C, y  a menos que no especifiquemos nada, es el que se utiliza siempre. [VER](./docs/context-propagation-and-baggage.md)
-
-12 - Configuración con variables de entorno: Dependen del lenguaje de programación, pero permiten configuarar aspectos del funcionamiento de Opentelemetry sin alterar el código
-
-13 - Performance: Permite modificar la cantidad de procesamiento requerido para analizar y generar los spans, tipos: Batch(default, producción), Simple(menos performante) Sin Procesador(debug) 
-
-
-
-
+## Opentelemetry Collector
+1 - Instalamos el colector y lo configuramos [ver](./docs/using-the-collector.md)
--- tracer.ts
+++ tracer.ts
@@ -2,69 +2,70 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-import { MeterProvider } from '@opentelemetry/sdk-metrics';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-// Manejo de Samples
 import { ParentBasedSampler, TraceIdRatioBasedSampler } from '@opentelemetry/sdk-trace-base'
-import { OurSampler } from './OurSampler';
-// Context Propagation
+import { OurSampler } from './ourSampler';
 import {W3CBaggagePropagator, W3CTraceContextPropagator, CompositePropagator} from '@opentelemetry/core'
+import { OTLPMetricExporter} from '@opentelemetry/exporter-metrics-otlp-proto'
 
 function start(serviceName: string) {
+
+    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
+    // const exporter = new PrometheusExporter({}, () => {
+    //     console.log(
+    //         `prometheus scrape endpoint: http://localhost:${port}${endpoint}`,
+    //     );
+    // });
+    const meterProvider = new MeterProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+        }),
+    }); 
+    const metricReader = new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({
+            url:'http://collector:4318/v1/metrics'
+        })
+    })
+    meterProvider.addMetricReader(metricReader);
+    const meter = meterProvider.getMeter('my-service-meter');
+
     const traceExporter = new OTLPTraceExporter({
-        url: 'http://jaeger:4318/v1/traces',
+        url: 'http://collector:4318/v1/traces',
     });
 
     const sdk = new NodeSDK({
         traceExporter,
-        serviceName:serviceName,
+        serviceName: serviceName,
         instrumentations: [getNodeAutoInstrumentations({
-            "@opentelemetry/instrumentation-fs":{           // Esta instrumentación es automática, por config podemos desactivarla
+            "@opentelemetry/instrumentation-fs":{
                 enabled:false
             },
-            // Lo agrego para visualizar context propagation, no necesario
-            "@opentelemetry/instrumentation-http":{
-                headersToSpanAttributes:{
-                    client:{
-                        requestHeaders:['tracestate','traceparent','baggage']
-                    },
-                    server:{
-                        requestHeaders:['tracestate','traceparent','baggage']
-                    }
-                }
-            }
+            // "@opentelemetry/instrumentation-http":{
+            //     headersToSpanAttributes:{
+            //         client:{
+            //             requestHeaders:['tracestate','traceparent','baggage']
+            //         },
+            //         server:{
+            //             requestHeaders:['tracestate','traceparent','baggage']
+            //         }
+            //     }
+            // }
         })],
-        // Agrego Resources automáticos y personalizados a los Spans
-        autoDetectResources: true,
+        autoDetectResources:true,
         resource: new Resource({
-          'team': 'Factory Backend',
-          'version': '2.17.0'
+            'team.owner':'core-team',
+            'deployment':'4'
         }),
-        // Agrego lógica que define cuándo generar muestras
         sampler: new ParentBasedSampler({
             root: new OurSampler()
         }),
-        // Context Propagation
         textMapPropagator: new CompositePropagator({
             propagators:[new W3CTraceContextPropagator(), new W3CBaggagePropagator()]
         })
-    });
 
-    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
-    const exporter = new PrometheusExporter({}, () => {
-        console.log(
-          `prometheus scrape endpoint: http://localhost:${port}${endpoint}`,
-        );
-      });
-    const meterProvider = new MeterProvider({
-            resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-        }),
-    });    
-    
-    meterProvider.addMetricReader(exporter);
-    const meter = meterProvider.getMeter('my-service-meter');
+    });
 
     sdk.start();
 
